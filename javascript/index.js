2e417d4f7b1bc68e13ee800bb679ca23e82f5a5d
Filipa <filipamnmlopes@hotmail.com>
2024-06-04 09:55:05 +0200

    added Josh way for Bonus

--- javascript/index.js
+++ javascript/index.js
@@ -116,7 +116,9 @@ async function doTheBroccoli() {
     document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
     // const step6 = await obtainInstruction("broccoli", 6);
     // document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
-    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector(
+      "#broccoli"
+    ).innerHTML += `<li>Broccoli is ready!</li>`;
     document.querySelector("#broccoliImg").removeAttribute("hidden");
   } catch (error) {
     console.log(error);
@@ -126,42 +128,78 @@ doTheBroccoli();
 
 // Bonus 2 - Promise all
 
-const promise1 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 0));
-})
-const promise2 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 1));
-})
-const promise3 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 2));
-})
-const promise4 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 3));
-})
-const promise5 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 4));
-})
-const promise6 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 5));
-})
-const promise7 = new Promise((resolve, _) => {
-  resolve(obtainInstruction("brusselsSprouts", 6));
-})
-// const promise8 = new Promise((resolve, _) => {
-//   resolve(obtainInstruction("brusselsSprouts", 7));
-// })
+// const promise1 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 0));
+// });
+// const promise2 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 1));
+// });
+// const promise3 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 2));
+// });
+// const promise4 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 3));
+// });
+// const promise5 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 4));
+// });
+// const promise6 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 5));
+// });
+// const promise7 = new Promise((resolve, _) => {
+//   resolve(obtainInstruction("brusselsSprouts", 6));
+// });
+// // const promise8 = new Promise((resolve, _) => {
+// //   resolve(obtainInstruction("brusselsSprouts", 7));
+// // })
 
-// ask chatGPT to improve the above code
+// // ask chatGPT to improve the above code above
 
-Promise.all( [promise1, promise2, promise3, promise4, promise5, promise6, promise7] )
-  .then((values) => {
-    console.log(values);
-    values.forEach(val => {
-      document.querySelector("#brusselsSprouts").innerHTML += `<li>${val}</li>`;
+// Promise.all([
+//   promise1,
+//   promise2,
+//   promise3,
+//   promise4,
+//   promise5,
+//   promise6,
+//   promise7,
+// ])
+//   .then((values) => {
+//     console.log(values);
+//     values.forEach((val) => {
+//       document.querySelector("#brusselsSprouts").innerHTML += `<li>${val}</li>`;
+//     });
+//     document.querySelector(
+//       "#brusselsSprouts"
+//     ).innerHTML += `<li>Brussels sprouts are ready!</li>`;
+//     document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//Josh way:
+
+const doBrussels = async () => {
+  const promiseArr = [];
+  for (let i = 0; i < brusselsSprouts.length; i++) {
+    promiseArr.push(obtainInstruction("brusselsSprouts", i));
+  }
+  Promise.all(promiseArr)
+    .then((promiseAllResponse) => {
+      console.log(promiseAllResponse);
+      promiseAllResponse.forEach((oneStep) => {
+        document.querySelector(
+          "#brusselsSprouts"
+        ).innerHTML += `<li>${oneStep}</li>`;
+      });
+      document.querySelector(
+        "#brusselsSprouts"
+      ).innerHTML += `<li>Brussels sprouts are ready!</li>`;
+      document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
     })
-    document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`;
-    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+    .catch((err) => {
+      console.group(error);
+    });
+};
+doBrussels()
